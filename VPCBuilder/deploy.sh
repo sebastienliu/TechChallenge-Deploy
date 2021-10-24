51a2dfde9197c6f4eed364a8a317ced46308cc96
Siwen LIU <2898299+sebastienliu@users.noreply.github.com>
2021-10-24 16:10:10 +1100

    Delete legacy commits

--- VPCBuilder/deploy.sh
+++
@@ -1,5 +0,0 @@
-aws --profile=default cloudformation create-stack \
---stack-name TechChallengeVPC \
---capabilities=CAPABILITY_IAM \
---template-body file://infra/vpc.yml \
---parameters ParameterKey=Environment,ParameterValue=Dev
